5ff05ae5e70e1c116e09bb6563ca18ed58cecae7
李晨 <lichen@Li-Chens-MacBook-Pro.local>
2020-02-28 12:09:44 +0800

    first bug version

---
+++ A1/GameClient.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+import socket
+import os.path
+import sys
+
+
+def send_msg(conn_socket, msg):
+    try:
+        conn_socket.send(msg.encode())
+    except socket.error as err:
+        print("Socket sending error: ", err)
+        sys.exit(1)
+
+
+def get_msg(conn_socket):
+    try:
+        msg = conn_socket.recv(1024).decode()
+    except socket.error as err:
+        print("Socket recv error: ", err)
+        sys.exit(1)
+    print(msg)
+    return msg.split()
+
+def main(argv):
+    # create socket and connect to server
+    try:
+        conn_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn_socket.connect((argv[1], int(argv[2])))
+    except socket.error as err:
+        print("Socket error: ", err)
+        sys.exit(1)
+
+    # user authentication
+    success_login = False
+    msg = None
+    while not success_login:
+        user_name = input("Please input your user name: ")
+        password = input("Please input your password: ")
+        send_msg(conn_socket, f"/login {user_name} {password}")
+        msg = get_msg(conn_socket)
+        if msg[0] == "1001":
+            success_login = True
+
+    # receive acknowledge - e.g., "OK"
+    while True:
+        user_input = input()
+        send_msg(conn_socket, user_input)
+        msg = get_msg(conn_socket)
+        if msg[0] == "4001":
+            print(msg)
+            break
+        elif msg[0] == "3011":
+            print(msg)
+            msg = get_msg(conn_socket)
+    print(msg)
+
+    print("Client ends")
+    conn_socket.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: python3 GameClient.py <server_addr> <server_port>")
+        sys.exit(1)
+    main(sys.argv)
---
+++ A1/GameServer.py
@@ -0,0 +1,284 @@
+#!/usr/bin/python3
+
+import random
+import socket
+import sys
+import threading
+
+TOTAL_ROOM = 10
+MAX_PLAYER_NUM = 100
+USER_INFO = {}
+MSG = {1001: "Authentication successful",
+       1002: "Authentication failed",
+       3001: "",
+       3011: "Wait",
+       3012: "Game started. Please guess true or false",
+       3013: "The room is full",
+       3021: "You are the winner",
+       3022: "You lost this game",
+       3023: "The result is a tie",
+       4001: "Buy bye",
+       4002: "Unrecognized message"
+       }
+
+
+class GameHouse(object):
+    def __init__(self, id):
+        self.id = id
+        self.player_val_pair = {}
+        self.rand_bool_val = -1     # 0: false, 1: true
+
+    def add_player(self, cur_player):
+        self.player_val_pair[cur_player] = -1
+
+    def find_partner(self, cur_player):
+        for player in self.player_val_pair:
+            if player != cur_player:
+                return player
+        return None
+
+    def set_player_val(self, cur_player, val):
+        self.player_val_pair[cur_player] = val
+
+    def generate_rand_bool(self):
+        if self.rand_bool_val == -1:
+            self.rand_bool_val = random.randint(0, 1)
+
+    def calc_game_res(self, cur_player):
+        # return result:
+        # -1 -> wait for partner,   0 -> current player loses,
+        # 1 -> current player wins, 2 -> tie
+        partner_val = self.player_val_pair[self.find_partner(cur_player)]
+        if partner_val == -1:
+            return -1
+        cur_val = self.player_val_pair[cur_player]
+        if cur_val == partner_val:
+            return 2
+        generate_rand_bool()
+        return self.rand_bool_val == cur_val
+
+    def reset(self):
+        self.player_val_pair = {}
+        self.rand_bool_val = -1
+
+
+class Player(object):
+    def __init__(self, name):
+        self.name = name
+        self.room = -1
+        self.status = 0
+        self.conn_socket = None
+        # status:
+        # 0 -> out of house,    1 -> in the game hall,
+        # 2 -> waiting in room, 3 -> playing a game
+    
+    def __eq__(self, other): 
+        if not isinstance(other, Player):
+            return False
+        return self.name == other.name
+    
+    def __hash__(self): 
+        return hash(self.name)
+
+    def login(self, conn_socket):
+        self.set_status(1)
+        self.conn_socket = conn_socket
+
+    def join_room(self, room_no):
+        self.room = room_no
+        self.set_status(2)
+
+    def end_game(self):
+        self.room = -1
+        self.set_status(1)
+
+    def exit_game(self):
+        self.room = -1
+        self.set_status(0)
+        self.conn_socket = None
+
+    def set_status(self, new_status):
+        self.status = new_status
+
+
+class Game(object):
+    def __init__(self, server_socket):
+        self.lock = threading.Lock()
+        self.server_socket = server_socket
+        self.game_houses = [GameHouse(id) for id in range(TOTAL_ROOM)]
+        self.players = [Player(pair[0]) for pair in USER_INFO]
+
+    def start_game(self):
+        while True:
+            client = self.server_socket.accept()
+            new_client = threading.Thread(
+                target=self.handle_each_client, args=(client,))
+            new_client.start()
+
+    def check_connection(self, connect, cur_player):
+        if connect:
+            return
+        if cur_player.status == 3:
+            with self.lock:
+                partner = self.game_houses[cur_player.room_no].find_partner(
+                    cur_player)
+            if partner:
+                send_msg(partner.conn_socket, 3021)
+        cur_player.exit_game()
+
+    def handle_each_client(self, client):
+        conn_socket, addr = client
+
+        # login authentication
+        auth_info = []
+        self.get_msg(conn_socket, auth_info)
+        while USER_INFO[auth_info[1]] != auth_info[2]:
+            self.send_msg(conn_socket, 1002)
+            self.get_msg(conn_socket, auth_info)
+
+        for player in self.players:
+            if player.name == auth_info[1]:
+                cur_player = player
+        cur_player.login(conn_socket)
+        connect = self.send_msg(conn_socket, 1001)
+        self.check_connection(connect, cur_player)
+
+        # login successful
+        while(cur_player.status != 0):
+            msg = []
+            connect = self.get_msg(conn_socket, msg)
+            self.check_connection(connect, cur_player)
+            action = self.parse_msg(msg, cur_player)
+            if action:
+                connect = self.send_msg(conn_socket, action)
+            self.check_connection(connect, cur_player)
+
+        conn_socket.close()
+
+    def get_msg(self, conn_socket, msg):
+        try:
+            str_msg = conn_socket.recv(1024).decode()
+        except socket.error as err:
+            print("Socket recv error: ", err)
+            conn_socket.close()
+            return False
+        msg[:] = list(str_msg.split())
+        return True
+
+    def send_msg(self, conn_socket, action):
+        msg = str(action) + " " + MSG[action]
+        if action == 3001:
+            with self.lock:
+                msg += str(TOTAL_ROOM) + ' '.join(
+                    [str(len(house.player_val_pair)) for house in self.game_houses])
+        try:
+            conn_socket.send(msg.encode())
+        except socket.error as err:
+            print("Socket sending error: ", err)
+            conn_socket.close()
+            return False
+        return True
+
+    def parse_msg(self, msg, cur_player):
+        status = cur_player.status
+        cur_player_room_no = cur_player.room
+        connect_partner = True
+        action = 4002
+        print("in parse:")
+        print(msg)
+        if (msg[0] == "/list" and len(msg) == 1):
+            action = 3001
+
+        elif (msg[0] == "/enter" and len(msg) == 2 and status == 1):
+            room_to_join = int(msg[1])
+            if not room_to_join or room_to_join <= 0 or room_to_join > TOTAL_ROOM:
+                return action
+
+            # calculate players ALREADY in the room
+            self.lock.acquire()
+            players_in_room = len(self.game_houses[room_to_join].player_val_pair)
+
+            if players_in_room < 2:
+                cur_player.join_room(room_to_join)
+                self.game_houses[room_to_join].add_player(cur_player)
+            if players_in_room == 1:
+                partner = self.game_houses[room_to_join].find_partner(
+                    cur_player)
+                cur_player.set_status(3)
+                partner.set_status(3)
+                connect_partner = self.send_msg(partner.conn_socket, 3012)
+            self.lock.release()
+
+            action = players_in_room + 3011
+
+        elif (msg[0] == "/guess" and status == 2):
+            if (msg[1] not in ["true", "false"]):
+                return action
+
+            self.lock.acquire()
+            cur_house = self.game_houses[cur_player_room_no]
+            cur_house.set_player_val(cur_player, int(msg[1] == "true"))
+            partner = cur_house.find_partner(cur_player)
+            result = cur_house.calc_game_res(cur_player)
+            action = 0
+            if result == -1:
+                return None
+            elif result == 0:
+                connect_partner = send_msg(partner.conn_socket, 3021)
+                action = 3022
+            elif result == 1:
+                connect_partner = send_msg(partner.conn_socket, 3022)
+                action = 3021
+            else:
+                connect_partner = send_msg(partner.conn_socket, 3023)
+                action = 3023
+
+            cur_house.reset()
+            self.lock.release()
+            cur_player.end_game()
+            partner.end_game()
+
+        elif msg[0] == "/exit" and len(msg) == 1 and status == 1:
+            cur_player.exit_game()
+            action = 4001
+
+        if not connect_partner:
+            action = 3021
+
+        return action
+
+
+def main(argv):
+    _, port, filepath = argv
+    global USER_INFO
+
+    # read user info from txt, create players
+    with open(filepath) as f:
+        for line in f:
+            pair = line.rstrip('\n').split(':')
+            USER_INFO[pair[0]] = pair[1]
+
+    print(USER_INFO)
+    # create socket and bind
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        server_socket.bind(("", int(port)))
+    except socket.error as err:
+        print("Binding error: ", err)
+        sys.exit(1)
+
+    server_socket.listen(MAX_PLAYER_NUM)
+
+    # start a new game from the server
+    new_game = Game(server_socket)
+    new_game.start_game()
+
+    server_socket.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: python3 GameServer.py <server_port> <path_to_user_file>")
+        sys.exit(1)
+    main(sys.argv)
